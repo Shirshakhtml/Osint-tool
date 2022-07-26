81dc829137e592216155deed6a2dcf4ca45316be
Shirshak Roy <75179984+Shirshakhtml@users.noreply.github.com>
2022-07-26 19:28:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,21 @@
-#!/usr/bin
-
+#!/bin/bash
 echo "[+] Sit back and relax till this installation finishes"
 sleep 2
-sudo apt install toilet figlet golang sublist3r nmap
-
+sudo apt install figlet;
+sudo apt install toilet;
+apt install golang;
+apt install nmap;
+apt install sublist3r
 sleep 2
-
 echo "[+] Installing Go tools..."
-
 go install github.com/tomnomnom/assetfinder@latest
-
 sleep 2
-
 go install github.com/tomnomnom/httprobe@latest
-
 sleep 4
-
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-
 sleep 2
-
 echo "[+] Transferring the Go binaries to /usr/bin"
 
-sudo cp /home/kali/go/bin/httpx /usr/bin
-
 sleep 2
 
-sudo cp /home/kali/go/bin/httprobe /usr/bin
-
-sleep 2
-
-sudo cp /home/kali/go/bin/assetfinder /usr/bin
+echo "Done"
